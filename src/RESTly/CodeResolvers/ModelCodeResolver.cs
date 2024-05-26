5071d1afc608d3b8f8042d63797d263e5e72c81f
Y56380X <y56380x@users.noreply.github.com>
2024-05-26 17:07:51 +0200

    resolve model subtypes

--- src/RESTly/CodeResolvers/ModelCodeResolver.cs
+++ src/RESTly/CodeResolvers/ModelCodeResolver.cs
@@ -1,6 +1,7 @@
 // Copyright Y56380X https://github.com/Y56380X/RESTly.
 // Licensed under the MIT License.
 
+using System.Text;
 using Microsoft.OpenApi.Models;
 
 namespace Restly.CodeResolvers;
@@ -18,9 +19,31 @@ internal sealed class ModelCodeResolver : CodeResolverBase
 	
 	protected override string Resolve()
 	{
-		var modelProperties = _schema.Properties.Select(GeneratePropertyCode);
-		return $"{"\t"}public record {_modelTypeName}({string.Join(",\n\t\t", modelProperties)});";
+		var modelProperties = _schema.Properties.Select(GeneratePropertyCode).ToArray();
+		var propertyPrefix = modelProperties.Length > 1 ? "\n\t\t" : string.Empty;
+		var modelCodeBuilder = new StringBuilder($"{"\t"}public record {_modelTypeName}({propertyPrefix}{string.Join(",\n\t\t", modelProperties)});");
+		
+		// Generate sub models
+		foreach (var property in _schema.Properties.Where(p => IsSubModel(p.Value)))
+		{
+			var subModelName = GenerateSubModelName(property.Key);
+			var subModelSchema = property.Value.Type == "array"
+				? property.Value.Items
+				: property.Value;
+			var codeResolver = new ModelCodeResolver(subModelName, subModelSchema);
+			modelCodeBuilder.AppendLine();
+			modelCodeBuilder.AppendLine();
+			modelCodeBuilder.Append(codeResolver.Resolve());
+		}
+		
+		return modelCodeBuilder.ToString();
 	}
+
+	private string GenerateSubModelName(string schemaName) => $"{_modelTypeName}{schemaName.Capitalize()}".NormalizeCsName();
+
+	private static bool IsSubModel(OpenApiSchema schema) => 
+		schema is { Type: "object", Reference: null } 
+			   or { Type: "array", Items: { Type: "object", Reference: null } };
 
 	private string GeneratePropertyCode(KeyValuePair<string, OpenApiSchema> property)
 	{
@@ -28,7 +51,11 @@ internal sealed class ModelCodeResolver : CodeResolverBase
 		var selectedPropertyName = normalizedPropertyName == _modelTypeName
 			? $"{normalizedPropertyName}_"
 			: normalizedPropertyName;
+
+		var propertyType = IsSubModel(property.Value)
+			? $"{GenerateSubModelName(property.Key)}{(property.Value.Type == "array" ? "[]" : string.Empty)}"
+			: property.Value.ToCsType();
 		
-		return $"[property:JsonPropertyName(\"{property.Key}\")]{property.Value.ToCsType()} {selectedPropertyName}";
+		return $"[property:JsonPropertyName(\"{property.Key}\")]{propertyType} {selectedPropertyName}";
 	}
 }
