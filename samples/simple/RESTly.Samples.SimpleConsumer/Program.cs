2ac9e77b26b824ed24873831d0de07684dcd7b24
Y56380X <y56380x@users.noreply.github.com>
2025-02-14 00:18:50 +0100

    add derived type and polymorph type sample code

--- samples/simple/RESTly.Samples.SimpleConsumer/Program.cs
+++ samples/simple/RESTly.Samples.SimpleConsumer/Program.cs
@@ -24,3 +24,15 @@ Console.WriteLine($"Cities: {string.Join(", ", citiesResult.Model ?? ArraySegmen
 var thingsResult = await simpleApi.QueryThingsByIndexAsync([1, 4]);
 Console.WriteLine(thingsResult);
 Console.WriteLine($"Things: {string.Join(", ", thingsResult.Model ?? ArraySegment<string?>.Empty)}");
+
+var floors = await simpleApi.GetFloorsAsync();
+Console.WriteLine(floors.Model?.FirstOrDefault());
+
+var polyItems1 = await simpleApi.GetDerivedTypesAsync();
+foreach (var polyItem in polyItems1.Model ?? [])
+	Console.WriteLine(polyItem);
+await simpleApi.PostDerivedTypesAsync(new SimpleApi.FinalType1("Name1", 100));
+await simpleApi.PostDerivedTypesAsync(new SimpleApi.FinalType2("Name1", 0.1d, 200d));
+var polyItems2 = await simpleApi.GetDerivedTypesAsync();
+foreach (var polyItem in polyItems2.Model ?? [])
+	Console.WriteLine(polyItem);
